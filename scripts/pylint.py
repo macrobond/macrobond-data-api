c72d24c3526a023ce4e49580786b69ff7474a495
mb-jp <99672670+mb-jp@users.noreply.github.com>
2022-12-11 12:14:17 +0100

    Update pylint.py

--- scripts/pylint.py
+++ scripts/pylint.py
@@ -4,7 +4,7 @@ from context import Context
 
 
 def pylint(context: Context) -> None:
-    context.python_run("pylint", " .\\macrobond_data_api\\ -f colorized -r y")
+    context.python_run("pylint", "macrobond_data_api -f colorized -r y")
 
 
 if __name__ == "__main__":
