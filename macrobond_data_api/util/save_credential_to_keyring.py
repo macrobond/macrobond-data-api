924dc8786d6adc1cf8802d0153385947e077f83d
mb-jp <Johan.Polson@macrobond.com>
2023-02-12 18:06:08 +0100

    fix save_credential_to_keyring

--- macrobond_data_api/util/save_credential_to_keyring.py
+++ macrobond_data_api/util/save_credential_to_keyring.py
@@ -29,8 +29,8 @@ def _inquiry(question: str, default: str = "yes") -> bool:
         sys.stdout.write("Please respond with 'yes' or 'no' (or 'y' or 'n').\n")
 
 
-def _remove_duplicates(service_name: str, warn_before_removing: bool) -> bool:
-    old_credential = keyring.get_credential(service_name, "")
+def _remove_duplicates(service_name: str, username: str, warn_before_removing: bool) -> bool:
+    old_credential = keyring.get_credential(service_name, username)
     while old_credential:
         if warn_before_removing and not _inquiry(
             'Warning - There is already a key with the same service name, it has the username "'
@@ -39,7 +39,7 @@ def _remove_duplicates(service_name: str, warn_before_removing: bool) -> bool:
         ):
             return False
         keyring.delete_password(service_name, old_credential.username)
-        old_credential = keyring.get_credential(service_name, "")
+        old_credential = keyring.get_credential(service_name, username)
     return True
 
 
@@ -94,37 +94,6 @@ def save_credential_to_keyring(warn_before_removing: bool = True, ask_for_servic
     is_darwin = sys.platform.startswith("darwin")
     keyring_name = keyring.get_keyring().name
 
-    def inquiry(question: str, default: str = "yes") -> bool:
-        valid = {"yes": True, "y": True, "no": False, "n": False}
-        if default is None:
-            prompt = " [y/n] "
-        elif default == "yes":
-            prompt = " [Y/n] "
-        elif default == "no":
-            prompt = " [y/N] "
-
-        while True:
-            sys.stdout.write(question + prompt)
-            choice = input().lower()
-            if default is not None and choice == "":
-                return valid[default]
-            if choice in valid:
-                return valid[choice]
-            sys.stdout.write("Please respond with 'yes' or 'no' (or 'y' or 'n').\n")
-
-    def remove_duplicates(service_name: str, username: str, warn_before_removing: bool) -> bool:
-        old_credential = keyring.get_credential(service_name, username)
-        while old_credential:
-            if warn_before_removing and not inquiry(
-                'Warning - There is already a key with the same service name, it has the username "'
-                + old_credential.username
-                + '" can we remove it ? '
-            ):
-                return False
-            keyring.delete_password(service_name, old_credential.username)
-            old_credential = keyring.get_credential(service_name, username)
-        return True
-
     print("Saving secret to " + keyring_name + "\n")
 
     service_name = (
@@ -139,7 +108,7 @@ def save_credential_to_keyring(warn_before_removing: bool = True, ask_for_servic
     if service_name == "":
         service_name = DEFAULT_SERVICE_NAME
 
-    if not remove_duplicates(service_name, DARWIN_USERNAME if is_darwin else "", warn_before_removing):
+    if not _remove_duplicates(service_name, DARWIN_USERNAME if is_darwin else "", warn_before_removing):
         return False
 
     username = input("Please enter Macrobond Web Api username: ")
