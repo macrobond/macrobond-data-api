09cb4a3c7f5b8b53830af282b622128bc523a056
Thomas Olsson <72909585+mb-to@users.noreply.github.com>
2023-04-27 15:21:03 +0200

    Removed user specific reference in test

--- tests/Com/test_macrobond_config.py
+++ tests/Com/test_macrobond_config.py
@@ -6,7 +6,7 @@ import pytest
 _winreg_import_error: Optional[ImportError] = None
 try:
     # winreg is not available on linux so mypy will fail on build server as it is runiong on linux
-    from winreg import OpenKey, QueryValueEx, HKEY_USERS  # type: ignore
+    from winreg import OpenKey, QueryValueEx, HKEY_CURRENT_USER  # type: ignore
 except ImportError as ex:
     _winreg_import_error = ex
 
@@ -19,11 +19,11 @@ def test_macrobond_config() -> None:
         raise _winreg_import_error
 
     sub_key = (
-        "S-1-5-21-1109962298-778639869-2696087834-1002\\Software\\Macrobond Financial\\"
+        "Software\\Macrobond Financial\\"
         + "Communication\\CommunicationState"
     )
 
-    with OpenKey(HKEY_USERS, sub_key) as regkey:
+    with OpenKey(HKEY_CURRENT_USER, sub_key) as regkey:
         val = QueryValueEx(regkey, "PreferredServer")
         assert val[0] in (
             "https://app.macrobondfinancial.com/app",
