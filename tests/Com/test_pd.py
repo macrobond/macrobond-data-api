cc05927984beb7acdd51cec377f2b845721da941
mb-jp <Johan.Polson@macrobond.com>
2023-12-18 10:49:32 +0100

    init

--- tests/Com/test_pd.py
+++ tests/Com/test_pd.py
@@ -3,6 +3,8 @@ from macrobond_data_api.com import ComClient
 
 
 def test_pd_datetime() -> None:
-    pd_datetime = pd.to_datetime("2022-07-01").tz_localize("utc")
+    pd_timestamp = pd.to_datetime("2022-07-01").tz_localize("utc")
+    datetime = pd_timestamp.to_pydatetime()
     with ComClient() as mda:
-        mda.get_observation_history("uslama4760", pd_datetime)
+        mda.get_observation_history("uslama4760", pd_timestamp)
+        mda.get_observation_history("uslama4760", datetime)
