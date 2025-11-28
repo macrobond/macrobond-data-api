66fbb16bded6edd1894fdfa1815259023442cc3f
Johan Polson <99672670+mb-jp@users.noreply.github.com>
2025-11-28 10:48:45 +0100

    Add AuthTooManyRequestsException (#258)

--- macrobond_data_api/web/auth_exceptions.py
+++ macrobond_data_api/web/auth_exceptions.py
@@ -12,3 +12,18 @@ class AuthFetchTokenException(AuthBaseException):
 
 class AuthInvalidCredentialsException(AuthFetchTokenException):
     pass
+
+
+class AuthTooManyRequestsException(AuthBaseException):
+
+    @property
+    def retry_after(self) -> int:
+        """
+        retry-after indicate how long a client should wait before making the request again.
+        this is in seconds.
+        """
+        return self._retry_after
+
+    def __init__(self, message: str, retry_after: int) -> None:
+        super().__init__(message)
+        self._retry_after = retry_after
